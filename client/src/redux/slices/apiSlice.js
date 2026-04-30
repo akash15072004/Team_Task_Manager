94ac44bd7d846d65b017b2f23de71e23b08a0b1a
Akash Chaudhary <144554996+akash15072004@users.noreply.github.com>
2026-04-30 22:47:16 +0530

    Refactor API URL handling in apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:8800/api";
-
-const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl:
+    import.meta.env.MODE === "development"
+      ? "/api"
+      : import.meta.env.VITE_API_URL + "/api",
+  credentials: "include",
+});
 
 export const apiSlice = createApi({
   baseQuery,
